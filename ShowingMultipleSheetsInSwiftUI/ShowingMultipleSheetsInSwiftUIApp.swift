8f43242fa03830855f2f394abec123ad0a503c58
Ramill <ramill_ibragimov@icloud.com>
2022-08-15 21:26:36 +0300

    Initial Commit

---
+++ ShowingMultipleSheetsInSwiftUI/ShowingMultipleSheetsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowingMultipleSheetsInSwiftUIApp.swift
+//  ShowingMultipleSheetsInSwiftUI
+//
+//  Created by Ramill Ibragimov on 15.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShowingMultipleSheetsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
